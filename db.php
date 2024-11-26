fc9b6cdb1941b2762b0c71eacc8242888ea21e1c
RoxineAnneRegalado <146078207+RoxineAnneRegalado@users.noreply.github.com>
2024-11-25 21:15:21 -0800

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "roxine";
+$password = "@roxine123!";
 $dbname = "movies_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
